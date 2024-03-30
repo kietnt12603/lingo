09dc20f1f7174f9906a9c80dec39c4e10cc7c25f
kietnt12603 <97238108+kietnt12603@users.noreply.github.com>
2024-03-30 17:06:02 +0700

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -21,4 +21,24 @@ export const quests = [
     title: "Earn 1000 XP",
     value: 1000,
   },
+  {
+    title: "Earn 2000 XP",
+    value: 2000,
+  },
+  {
+    title: "Earn 5000 XP",
+    value: 5000,
+  },
+  {
+    title: "Earn 10000 XP",
+    value: 10000,
+  },
+  {
+    title: "Earn 15000 XP",
+    value: 15000,
+  },
+  {
+    title: "Earn 20000 XP",
+    value: 20000,
+  },
 ];
